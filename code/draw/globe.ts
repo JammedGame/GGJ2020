053b0fa9b463960d0061a959cf3d2f7793006d84
Milos Manojlovic <manojlovicsmilos@hotmail.com>
2020-02-01 17:06:57 +0100

    new earth textures

--- code/draw/globe.ts
+++ code/draw/globe.ts
@@ -9,16 +9,22 @@ class Globe
     private _material: Three.MeshStandardMaterial;
     private _sphereGeometry: Three.Geometry;
     private _colorMap: Three.Texture;
+    private _normalMap: Three.Texture;
+    private _specularMap: Three.Texture;
     private _bumpMap: Three.Texture;
     public get instance(): Three.Mesh { return this._mesh; }
     public constructor()
     {
         this._colorMap = new Three.TextureLoader().load("../../resources/earth.jpg");
+        this._normalMap = new Three.TextureLoader().load("../../resources/normal.jpg");
+        this._specularMap = new Three.TextureLoader().load("../../resources/specular.jpg");
         this._bumpMap = new Three.TextureLoader().load("../../resources/bump.jpg");
         this._material = new Three.MeshStandardMaterial({
             map: this._colorMap,
+            normalMap: this._normalMap,
+            emissiveMap: this._specularMap,
             displacementMap: this._bumpMap,
-            displacementScale: 0.05
+            displacementScale: 0.1
         });
         this._sphereGeometry = new Three.SphereGeometry(
             GLOBE_SCALE,
