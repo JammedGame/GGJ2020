d840b0e01435c9ba629f69b6d83e02b696f14a55
Nikola <glamdring0@hotmail.com>
2020-02-01 18:19:26 +0100

    ApiCall

--- code/logic.ts
+++ code/logic.ts
@@ -2,6 +2,7 @@ import { Renderer } from "./draw/renderer"
 import { Scene } from "./draw/scene";
 import { Tile, Tilemap } from "./tilemap/tilemap"
 import { World } from "./draw/world";
+import { Api } from "./data/api";
 
 export { GameLogic }
 
@@ -9,13 +10,16 @@ class GameLogic
 {
     private _scene: Scene;
 	private _renderer: Renderer;
-	private _tilemap: Tilemap;
+    private _tilemap: Tilemap;
+    private _apiData: any;
     public constructor()
     {
         this._renderer = new Renderer('canvas-parent');
         this._scene = new World();
 		this._renderer.setActiveScene(this._scene);
-		this._tilemap = new Tilemap(64, 32);
+        this._tilemap = new Tilemap(64, 32);
+        let api = new Api();
+        this._apiData = api.getApiData('Belgrade', 'Central Serbia', 'Serbia');
     }
     public run()
     {
