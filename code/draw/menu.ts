3e073f941e49f180fd60ab81acf941e7de6fac9f
Filip <filipvabramovic@gmail.com>
2020-02-02 15:41:58 +0200

    audio wip

--- code/draw/menu.ts
+++ code/draw/menu.ts
@@ -4,6 +4,20 @@ import { Scene } from "./scene";
 import { Camera } from "./camera";
 import { CANVAS_PARENT, MENU_DIV } from "../data/constants";
 import { Settings } from "../settings";
+import { Howl, Howler } from 'howler';
+
+const audioMenuBackground = new Howl({
+	src: ['./resources/MenuBackground.mp3'],
+	loop: true
+});
+const audioInGameBackgtound = new Howl({
+	src: ['./resources/InGameBackgtound.mp3'],
+	loop: true,
+	volume: 0.3
+});
+const audioButton = new Howl({
+	src: ['./resources/Button.wav'],
+});  
 
 class Menu extends Scene
 {
@@ -17,6 +31,7 @@ class Menu extends Scene
 		// override
 		this._menuDiv = <HTMLDivElement> document.getElementById(MENU_DIV);
 		Settings.menuClick = this.onClick.bind(this);
+		audioMenuBackground.play();
     }
 
     public update()
@@ -39,7 +54,10 @@ class Menu extends Scene
 			case 'menu-start-game':
 				Settings.inMenu = false;
 				this.toggleShown(false);
-				break;
+				audioMenuBackground.stop();
+				audioButton.play();
+				audioInGameBackgtound.play();
+				break;				
 		}
 	}
 }
--- code/simulation/tilemap.ts
+++ code/simulation/tilemap.ts
@@ -13,9 +13,14 @@ import {
 } from "../data/constants";
 import { Tile } from "./tile"
 import { Vector2 } from "three";
+import { Howl, Howler } from 'howler';
 
 export { Tilemap }
 
+const audioPathClosed = new Howl({
+	src: ['./resources/PathClosed.mp3'],
+	volume: 1
+});
 class Tilemap {
 	private readonly width: number;
 	private readonly height: number;
@@ -178,6 +183,8 @@ class Tilemap {
 				}
 
 			this.clearTrail();
+			audioPathClosed.play();
+			console.log(audioPathClosed);
 		}
 	}
 
@@ -335,7 +342,7 @@ class Tilemap {
 
 		let simulateEnd: number = performance.now();
 		if (this.debuggingEnabled) {
-			console.log("simulate execution time " + (simulateEnd - simulateStart) + " milliseconds");
+			// console.log("simulate execution time " + (simulateEnd - simulateStart) + " milliseconds");
 		}
 	}
 }
