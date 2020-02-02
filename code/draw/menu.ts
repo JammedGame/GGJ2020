5d501bc0ce72c03b122b71a3e55d2ea396fb9d1d
Djordje Cvijic <djordjec@nordeus.com>
2020-02-02 15:18:50 +0100

    main menu ui dunzo

--- code/draw/menu.ts
+++ code/draw/menu.ts
@@ -22,6 +22,10 @@ const audioButton = new Howl({
 class Menu extends Scene
 {
 	private _menuDiv: HTMLDivElement;
+	private _tutorial1Div: HTMLDivElement;
+	private _tutorial2Div: HTMLDivElement;
+	private _tutorial3Div: HTMLDivElement;
+	private _creditsDiv: HTMLDivElement;
     public constructor(camera: Camera)
     {
         super(camera);
@@ -30,6 +34,10 @@ class Menu extends Scene
     {
 		// override
 		this._menuDiv = <HTMLDivElement> document.getElementById(MENU_DIV);
+		this._tutorial1Div = this._menuDiv.querySelector('#tutorial-1-div');
+		this._tutorial2Div = this._menuDiv.querySelector('#tutorial-2-div');
+		this._tutorial3Div = this._menuDiv.querySelector('#tutorial-3-div');
+		this._creditsDiv = this._menuDiv.querySelector('#credits-div');
 		Settings.menuClick = this.onClick.bind(this);
 		audioMenuBackground.play();
     }
@@ -46,10 +54,13 @@ class Menu extends Scene
 	{
 		// override
 		this._menuDiv.style.display = shown ? 'block' : 'none';
+		this._tutorial1Div.style.display = "none";
+		this._tutorial2Div.style.display = "none";
+		this._tutorial3Div.style.display = "none";
+		this._creditsDiv.style.display = "none";
 	}
 	private onClick(target: any): void {
 		console.log(target);
-		console.log(typeof target);
 		switch (target.id) {
 			case 'menu-start-game':
 				Settings.inMenu = false;
@@ -58,6 +69,26 @@ class Menu extends Scene
 				audioButton.play();
 				audioInGameBackgtound.play();
 				break;				
+			case 'menu-tutorial':
+				this._tutorial1Div.style.display = "block";
+				break;
+			case 'tutorial-1-div':
+				this._tutorial1Div.style.display = "none";
+				this._tutorial2Div.style.display = "block";
+				break;
+			case 'tutorial-2-div':
+				this._tutorial2Div.style.display = "none";
+				this._tutorial3Div.style.display = "block";
+				break;
+			case 'tutorial-3-div':
+				this._tutorial3Div.style.display = "none";
+				break;
+			case 'menu-credits':
+				this._creditsDiv.style.display = "block";
+				break;
+			case 'credits-div':
+				this._creditsDiv.style.display = "none";
+				break;
 		}
 	}
 }
